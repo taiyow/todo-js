beb413dc92da9645576578a9ee512185ddc5e500
taiyow <taiyo.w@gmail.com>
2024-09-08 22:41:59 +0900

    "36.タスクテキストの追加" の js を実装

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
 import './style.css' 
 
+const onClickAdd = () => {
+    // テキストボックスの値を取得し、クリアする
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+
+    // li 生成
+    const li = document.createElement("li");
+
+    // div 生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    // p 生成
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.innerText = inputText;
+
+    // li タグの子要素に各要素を設定
+    div.appendChild(p);
+    li.appendChild(div);
+
+    // 未完了リストに追加
+    document.getElementById("incomplete-list").appendChild(li)
+}
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
